9954f1e167c9f46edc19e8587b17cdd8683a4021
Uwe Voelker <uwe@uwevoelker.de>
2020-12-11 10:11:39 +0100

    commit local changes

--- temperature-logger.go
+++ temperature-logger.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"regexp"
 	"strconv"
@@ -12,17 +14,64 @@ import (
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
+func bp2string(bp client.BatchPoints) (bytes.Buffer, error) {
+	// https://github.com/influxdata/influxdb1-client/blob/master/v2/client.go#L367
+	var b bytes.Buffer
+
+	for _, p := range bp.Points() {
+		if p == nil {
+			continue
+		}
+		if _, err := b.WriteString(p.PrecisionString(bp.Precision())); err != nil {
+			return b, err
+		}
+
+		if err := b.WriteByte('\n'); err != nil {
+			return b, err
+		}
+	}
+
+	return b, nil
+}
+
+func write2spool(bp client.BatchPoints, spool string) {
+	// each hour a new file
+	t := time.Now()
+	filename := spool + "/" + t.Format("2006010215") + ".influx"
+
+	// append datapoints
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening spoolfile ", filename, ": ", err.Error())
+		return
+	}
+	defer f.Close()
+
+	buf, err := bp2string(bp)
+	if err != nil {
+		fmt.Println("Error converting datapoints to buffer: ", err.Error())
+		return
+	}
+
+	if _, err := f.Write(buf.Bytes()); err != nil {
+		fmt.Println("Error writing to spoolfile ", filename, ": ", err.Error())
+		return
+	}
+}
+
 func main() {
 	// process commandline args
 	var host string
 	var port string
 	var database string
 	var datapoint string
+	var spool string
 	var sleep int
 	flag.StringVar(&host, "host", "127.0.0.1", "InfluxDB host")
 	flag.StringVar(&port, "port", "8086", "InfluxDB port")
 	flag.StringVar(&database, "database", "temperature", "InfluxDB database")
 	flag.StringVar(&datapoint, "datapoint", "temp", "InfluxDB datapoint name")
+	flag.StringVar(&spool, "spool", "", "spool directory")
 	flag.IntVar(&sleep, "sleep", 20, "sleep between measurements (in seconds)")
 	flag.Parse()
 
@@ -94,7 +143,12 @@ func main() {
 			bp.AddPoint(pt)
 		}
 
-		influx.Write(bp)
+		err = influx.Write(bp)
+		if err != nil {
+			if spool != "" {
+				write2spool(bp, spool)
+			}
+		}
 
 		time.Sleep(time.Duration(sleep) * time.Second)
 	}
